2eb35e0494ba80135b26c5b50f3f23679b13703a
manish <manishkumar60708090@gmail.com>
2023-01-05 23:22:38 +0530

    Added check conversations and styled conversation sidebar item

--- src/utils/styles/index.tsx
+++ src/utils/styles/index.tsx
@@ -1,8 +1,6 @@
 import styled, { css } from 'styled-components';
 import { PageProps } from './styleTypes';
-
 export const SIDEBAR_WIDTH = 400;
-
 export const InputField = styled.input`
   font-family: 'Inter';
   outline: none;
@@ -48,7 +46,6 @@ export const Button = styled.button`
     background-color: #3a1cff;
   }
 `;
-
 export const Page = styled.div<PageProps>`
   background-color: #1a1a1a;
   height: 100%;
@@ -56,7 +53,6 @@ export const Page = styled.div<PageProps>`
   justify-content: ${(props) => props.justifyContent};
   align-items: ${(props) => props.alignItems};
 `;
-
 export const ConversationSidebarStyle = styled.aside`
   position: absolute;
   top: 0;
@@ -65,21 +61,43 @@ export const ConversationSidebarStyle = styled.aside`
   width: ${SIDEBAR_WIDTH}px;
   background-color: #1a1a1a;
   border-right: 1px solid #5454543d;
-  & header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 24px;
-    background-color: #151515;
-    height: 100px;
-    border-bottom: 1px solid #5454543d;
-    & h1 {
-      font-weight: 400;
-    }
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const ConversationSidebarHeader = styled.header`
+  position: fixed;
+  width: ${SIDEBAR_WIDTH}px;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 32px;
+  box-sizing: border-box;
+  background-color: #151515;
+  height: 100px;
+  border-bottom: 1px solid #5454543d;
+  & h1 {
+    font-weight: 400;
   }
 `;
 
 export const ConversationChannelPageStyle = styled.div`
   height: 100%;
   margin-left: ${SIDEBAR_WIDTH}px;
+`;
+
+export const ConversationSidebarContainer = styled.div``;
+
+export const ConversationSidebarItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  padding: 18px 32px;
+  box-sizing: border-box;
+  border-bottom: 1px solid #5454543d;
+  background-color: #131313;
 `;
