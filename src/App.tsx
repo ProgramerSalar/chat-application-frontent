a5ed3b59dfb74b4409c5198669242afeb07d2f9f
manish <manishkumar60708090@gmail.com>
2023-01-05 21:22:52 +0530

    create Register and RegisterForm components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import './App.css';
-import {Outlet , Route , Routes} from "react-router-dom";
+import { Outlet, Route, Routes } from 'react-router-dom';
+import { AuthenticationPage } from './pages/RegisterPage';
 
 function App() {
   return (
-    <div>
-      <p> Home </p>
-
-
-
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<AuthenticationPage />}></Route>
+        <Route
+          path="conversations"
+          element={
+            <div>
+              <div>Conversations</div>
+              <Outlet />
+            </div>
+          }
+        >
+          <Route path=":id" element={<div>Conversation ID Page</div>} />
+        </Route>
+      </Routes>
+    </>
   );
 }
-
 export default App;
